f6091f5e774b39859031b0d828f806873c477f95
CibeleMartins <cibelemartins24@hotmail.com>
2023-02-01 10:41:07 -0300

    🎉 iniciou o projeto

---
+++ src/main/java/br/com/personal/expenses/personalexpenses/PersonalExpensesApplication.java
@@ -0,0 +1,13 @@
+package br.com.personal.expenses.personalexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/personal/expenses/personalexpenses/PersonalExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personal.expenses.personalexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
