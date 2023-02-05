f479f3abfa7246e8dbcdc47e6dc073d04814594f
CibeleMartins <cibelemartins24@hotmail.com>
2023-02-05 17:11:46 -0300

    🚧 feat: add repository p/ usuário

---
+++ src/main/java/br/com/personal/expenses/personalexpenses/domain/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package br.com.personal.expenses.personalexpenses.domain.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.personal.expenses.personalexpenses.domain.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    
+    
+    // todos as propriedades do usuário podem se tornar um método de pesquisa no bd
+
+    // Nesse caso, vamos precisar desse método no momento da autenticação, 
+    // porque vamos precisar saber quem é o usuário no bd pelo email
+    List<User> findByEmail(String email);
+}
