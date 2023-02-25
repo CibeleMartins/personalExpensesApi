9f9e8d942f34061084985fadbdd444c52913a527
Cibele Martins <cibelemartins24@hotmail.com>
2023-02-25 17:53:58 -0300

    🚧 feat: criou modelos de requisição e resposta para o login

---
+++ src/main/java/br/com/personal/expenses/personalexpenses/dto/User/UserLoginRequest.java
@@ -0,0 +1,26 @@
+package br.com.personal.expenses.personalexpenses.dto.User;
+
+public class UserLoginRequest {
+    
+    private String email;
+
+    private String passwordUser;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordUser() {
+        return passwordUser;
+    }
+
+    public void setPasswordUser(String passwordUser) {
+        this.passwordUser = passwordUser;
+    }
+
+    
+}
---
+++ src/main/java/br/com/personal/expenses/personalexpenses/dto/User/UserLoginResponse.java
@@ -0,0 +1,26 @@
+package br.com.personal.expenses.personalexpenses.dto.User;
+
+public class UserLoginResponse {
+    
+    private String tokenAuthorization;
+
+    private UserResponseDTO user;
+
+    public String getTokenAuthorization() {
+        return tokenAuthorization;
+    }
+
+    public void setTokenAuthorization(String tokenAuthorization) {
+        this.tokenAuthorization = tokenAuthorization;
+    }
+
+    public UserResponseDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserResponseDTO user) {
+        this.user = user;
+    }
+
+    
+}
