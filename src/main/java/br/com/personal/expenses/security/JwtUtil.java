e056d93a803fc359c73eec4ab33bb09c1e0e4972
Cibele Martins <cibelemartins24@hotmail.com>
2023-02-25 15:28:08 -0300

    🚧 feat: add método que gera token para usuário logado

--- src/main/java/br/com/personal/expenses/security/JwtUtil.java
+++ src/main/java/br/com/personal/expenses/security/JwtUtil.java
@@ -1,10 +1,15 @@
 package br.com.personal.expenses.security;
 
+import java.security.Key;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import  br.com.personal.expenses.personalexpenses.domain.model.UserAdmin;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 // para utilizar a classe de maneira útil em toda aplicação
 @Component
@@ -31,7 +36,36 @@ public class JwtUtil {
         // método get() retorna os milisegundos da data atual 
         // pega a data atual e soma mais um dia em milisegundos
 
-        
+        // saber quem é o usuário que está gerando o token?
+        UserAdmin userAdmin = (UserAdmin) authentication.getPrincipal();
+        // getPrincipal() devolve um objeto, por isso um cast
+        // para o que vier no objeto ser convertido em um usuário
+        // assim, pega o usuário autenticado
+
+        // cria o token
+
+        try {
+
+            // gera a chave para o padrão de criptografia do token
+            Key secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes("UTF-8"));
+            // esse método recebe um array de bytes e transforma numa chave
+
+            // jwt tem um builder que recebe algumas propriedades
+            return Jwts.builder().setSubject(userAdmin.getUsername()).setIssuedAt(new Date()).setExpiration(dateExpiration).signWith(secretKey).compact();
+            // setSubject() recebe o dono do token, mas ele espera receber um string
+            // por isso, foi passado um valor único que identifique o usuário
+            // setIssuedAt() recebe a data que o token é gerado
+            // setExpiration() recebe a data de expiração do token
+            // compact() gera o token, pega tudo e compacta gerando uma string
+            // mas é necessário definir o padrão de criptografia que será usado na geração do token
+            // para isso, o signWith()
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+            return "";
+        }
+
     }
     
 
